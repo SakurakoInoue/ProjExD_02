379c3df04016114704b9e0ae6bce315bdba85a39
inouesan <c0b220159b@edu.teu.ac.jp>
2023-11-28 14:14:38 +0900

    爆弾の速度

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -16,9 +16,10 @@ def main():
     bb_img = pg.Surface((20,20)) #練習1：透明のSurfaceを作る
     bb_img.set_colorkey((0, 0, 0))
     pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 7) #練習1：赤い半径10の円を描く
-    bb_rct = bb_img.get_rect() #練習2：爆弾SurfaceのRectを抽出する
+    bb_rct = bb_img.get_rect() #練習1：爆弾SurfaceのRectを抽出する
     bb_rct.centerx = random.randint(0, WIDTH)
     bb_rct.centery = random.randint(0, HEIGHT)
+    vx, vy = +5, +5 #練習2：爆弾の速度
     
     clock = pg.time.Clock()
     tmr = 0
@@ -29,6 +30,7 @@ def main():
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
+        bb_rct.move_ip(vx, vy)
         screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
